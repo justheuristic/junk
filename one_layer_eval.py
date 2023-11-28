9c31735b976f830c596b91aaa7d7fad5300e1f95
vahe1994 <vahe527887@yandex.ru>
2023-11-28 21:24:30 +0400

    added quantization for one layer

--- one_layer_eval.py
+++ one_layer_eval.py
@@ -9,76 +9,89 @@ import torch.nn.functional as F
 import transformers
 
 from src.aq import QuantizedWeight, _reconstruct_weight  # see adjacent file (aq.py)
-from src.utils import  calc_avg_bits, get_mean_nbits_by_codebook  # see adjacent file (aq.py)
+from src.utils import calc_avg_bits, get_mean_nbits_by_codebook  # see adjacent file (aq.py)
 
 try:
     import wandb
+
     has_wandb = True
 except ModuleNotFoundError:
     has_wandb = False
 
 
 def calculating_XTX(args):
-    '''
+    """
     Calculating XTX and reference weight
-    '''
-    X = torch.load(args.input_loading_dir,
-               map_location='cpu').float().flatten(0, -2)
+    """
+    X = torch.load(args.input_loading_dir, map_location="cpu").float().flatten(0, -2)
     XTX = torch.zeros(X.shape[-1], X.shape[-1], device=device, dtype=torch.float64)
     for i in range(0, len(X), args.batch_size):
-        x_batch = X[i: i + args.batch_size].cuda().double()
-        XTX.addmm_(x_batch.T, x_batch, alpha=1/len(X))
+        x_batch = X[i : i + args.batch_size].cuda().double()
+        XTX.addmm_(x_batch.T, x_batch, alpha=1 / len(X))
         del x_batch
     XTX = XTX.float()
     del X
     return XTX
 
-def compress(args,run):
+
+def compress_aq(args, reference_weigh):
     quantized_layer = QuantizedWeight(
-        weight_shape=reference_weight.shape, num_codebooks=args.num_codebooks,
+        weight_shape=reference_weight.shape,
+        num_codebooks=args.num_codebooks,
         nbits_per_codebook=args.nbits_per_codebook,
-        out_group_size=args.out_group_size, in_group_size=args.in_group_size, 
+        out_group_size=args.out_group_size,
+        in_group_size=args.in_group_size,
         device=device,
-        init_kmeans=args.kmeans_init, reference_weight=reference_weight, alpha=1.0, verbose=True
+        init_kmeans=args.kmeans_init,
+        reference_weight=reference_weight,
+        alpha=1.0,
+        verbose=True,
     )
 
-    opt = torch.optim.Adam(quantized_layer.parameters(), lr=1e-5, betas=(0.9, 0.95))
+    opt = torch.optim.Adam(quantized_layer.parameters(), lr=args.lr, betas=(0.9, 0.95))
 
     for epoch in range(args.num_epochs):
         start = time.perf_counter()
-        
+
         reconstructed_weight = _reconstruct_weight(quantized_layer.codes, quantized_layer.codebooks)
         delta_weight = (reconstructed_weight - reference_weight).double()
         loss = (delta_weight @ XTX.double()).flatten() @ delta_weight.flatten() / len(delta_weight)
         opt.zero_grad()
         loss.backward()
         opt.step()
-        
-        run.log({'loss':loss.item()}, step=epoch)
-        
+
+        wandb.log({"loss": loss.item()}, step=epoch)
+
         if epoch % args.print_frequency == 0:
-            print(f"loss={loss.item():.10f}\t",
-                f"time_on_epoch {epoch} = {time.perf_counter() - start}")
+            print(f"loss={loss.item():.10f}\t", f"time_on_epoch {epoch} = {time.perf_counter() - start}")
         if (epoch + 1) % args.beam_search_epochs == 0:
             if (epoch + 1) % args.big_beam_search_epochs == 0:
                 print("BIG beam search")
             quantized_layer.requantize_(
-                XTX, reference_weight,
-                beam_size=args.beam_size if (epoch + 1) % 1000 != 0 else big_beam_size,
+                XTX,
+                reference_weight,
+                beam_size=args.beam_size if (epoch + 1) % args.big_beam_search_epochs != 0 else args.big_beam_size,
                 sparsity_regularizer=args.sparsity_regularizer,  # tip: use const_hparam * quantized_layer.codes.numel()
-                verbose=True)
+                verbose=True,
+            )
             if args.sparsity_regularizer != 0:
                 sparsity_rate = ((quantized_layer.codes == 0).sum() / quantized_layer.codes.numel()).item()
-                print(f"Sparsity rate {sparsity_rate:.5f}")
-                run.log({'sparsity rate': sparsity_rate}, step=epoch)
+                wandb.log({"sparsity rate": sparsity_rate}, step=epoch)
                 if (epoch + 1) % args.big_beam_search_epochs == 0:
-                    mean_code_nbits = sum(get_mean_nbits_by_codebook(quantized_layer.codes)) / num_codebooks
-                    print(f"mean_code_nbits {mean_code_nbits:.5f}")
-                    run.log({'Mean codebook ldngth nbits': mean_code_nbits}, step=epoch)
-                    if args.in_group_size>1 and args.out_group_size>1:
-                        curr_avg_bits  = calc_avg_bits(args.num_codebooks, 1, mean_code_nbits,
-                                            args.nbits_per_codebook, reference_weight.shape[0],reference_weight.shape[1])
-                        run.log({"Avg_bits": curr_avg_bits}, step=epoch)
+                    mean_code_nbits = sum(get_mean_nbits_by_codebook(quantized_layer.codes)) / args.num_codebooks
+                    wandb.log({"Mean codebook ldngth nbits": mean_code_nbits}, step=epoch)
+                    if args.in_group_size > 1 and args.out_group_size > 1:
+                        curr_avg_bits = calc_avg_bits(
+                            args.num_codebooks,
+                            1,
+                            mean_code_nbits,
+                            args.nbits_per_codebook,
+                            reference_weight.shape[0],
+                            reference_weight.shape[1],
+                        )
+                        wandb.log({"Avg_bits": curr_avg_bits}, step=epoch)
+    return quantized_layer
+
 
 if __name__ == "__main__":
     import argparse
@@ -97,12 +110,18 @@ if __name__ == "__main__":
         help="Number of epochs.",
     )
     parser.add_argument(
+        "--lr",
+        type=float,
+        default=1e-5,
+        help="relative threshold for     outliers; higher threshold = more outliers.",
+    )
+    parser.add_argument(
         "--num_codebooks",
         type=int,
         default=7,
         help="#Number of codebooks.",
     )
-   
+
     parser.add_argument(
         "--out_group_size",
         type=int,
@@ -178,18 +197,28 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.dataset == "custom":
-        print(
-            "WARNING: `--custom_data_path` argument and `--dataset=custom` option are DEPRECATED. ",
-            "Pass dataset path directly to `dataset` argument or use 'pajama', 'refinedweb'",
-            "See README.md for examples.",
-        )
-        args.dataset = args.custom_data_path
+
+    torch.set_num_threads(16)
+    torch.backends.cudnn.allow_tf32 = False
+    torch.backends.cuda.matmul.allow_tf32 = False
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print("\n============ Load model... ============")
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        args.model_path, torch_dtype="auto", low_cpu_mem_usage=True
+    )
+    reference_weight = model.model.layers[args.block_number].self_attn.q_proj.weight.detach().to(device).float()
+    del model
+
+    in_features, out_features  = reference_weight.shape[0],reference_weight.shape[1]
+    estimated_bits_per_param = calc_avg_bits(args.num_codebooks, args.out_group_size, args.in_group_size,
+                                                args.nbits_per_codebook, in_features,out_features)
+    print("Estimated bits / param", estimated_bits_per_param)
 
     if args.wandb:
         assert has_wandb, "`wandb` not installed, try pip install `wandb`"
         args.exp_name = (
-            os.environ.get("WANDB_NAME", "SpQR_run")
+            os.environ.get("WANDB_NAME", "AQ")
             + f"_num_codebooks_{args.num_codebooks}"
             + f"_out_group_size_{args.out_group_size}"
             + f"_in_group_size_{args.in_group_size}"
@@ -198,28 +227,18 @@ if __name__ == "__main__":
             + f"_big_beam_search_epochs_{args.big_beam_search_epochs}"
         )
         wandb.init(
-            dir =os.getcwd(),
+            dir=os.getcwd(),
             config={a: getattr(args, a) for a in dir(args) if not a.startswith("_")},
             settings=wandb.Settings(code_dir="."),
             save_code=True,
             project="AddQuantization",
-            entity = "rock-and-roll",
+            entity="rock-and-roll",
         )
-        # wandb.run.log_code(".")
 
-    torch.set_num_threads(16)
-    torch.backends.cudnn.allow_tf32 = False
-    torch.backends.cuda.matmul.allow_tf32 = False
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print("\n============ Load model... ============")
-    model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path,
-                                                          torch_dtype='auto', low_cpu_mem_usage=True)
-    reference_weight = model.model.layers[args.block_number].self_attn.q_proj.weight.detach().to(device).float()
-    del model
+        wandb.log({"Avg_bits": estimated_bits_per_param})
 
     print("============  Calculating XTX ... ============")
-    XTX  = calculating_XTX(args)
+    XTX = calculating_XTX(args)
+
     print("\n============ Quantizing layer... ============")
-    
-    print("\n============ Evaluating perplexity... ============")
-    torch.cuda.reset_peak_memory_stats()
+    compress_aq(args,reference_weight)
