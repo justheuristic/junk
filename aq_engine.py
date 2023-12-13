0659cda1898e70bf3aa4f036bcb5ebfdd3b0a828
Your Name <justheuristic@gmail.com>
2023-12-13 05:32:53 +0300

    typo

--- aq_engine.py
+++ aq_engine.py
@@ -168,5 +168,7 @@ class AQUtil(nn.Module):
 
 def replace_parameter_(module: nn.Module, name: str, new_value: torch.Tensor):
     """A hacky way to substitute an already registered parameter with a non-parameter tensor. Breaks future use."""
-    assert name in module._parameters, (name, module._parameters)
-    module._parameters[name] = new_value
+    if name in module._parameters:
+        module._parameters[name] = new_value
+    else:
+        setattr(module, name, new_value)
