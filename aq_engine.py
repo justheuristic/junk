399941b18597adf05bd1595bd4760fa65fd0f6fc
Your Name <justheuristic@gmail.com>
2023-12-13 05:20:45 +0300

    inherit nn.Module

--- aq_engine.py
+++ aq_engine.py
@@ -72,7 +72,7 @@ class AQUtil(nn.Module):
 
         replicas = None
         if len(args.devices) > 1:
-            replicas = torch.nn.parallel.replicate(args.devices)
+            replicas = torch.nn.parallel.replicate(self, args.devices)
             replicas[0] = self
 
         for epoch in range(args.num_epochs):
